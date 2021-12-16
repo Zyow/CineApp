3ae13cd4f4fad978b99f2777798667fa24fe80c9
Zyow <Szczypamaxime@gmail.com>
2021-12-16 16:40:15 +0100

    First commit

---
+++ src/test/java/fr/mxsz/CineApp/CineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mxsz.CineApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
